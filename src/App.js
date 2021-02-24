7dfa6bedcdcd5b3694bd03d5421b80c3c3a2bac5
MarinaYur <marina.yurkev@gmail.com>
2021-02-24 12:47:08 +0300

    feat: add timer setting

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
 import React from 'react';
 import './App.css';
-
+let timer = () => {};
 class App extends React.Component {
   constructor (props) {
     super(props);
-    this.value = [];
-    this.adressOfImages = [];
+    this.twoOpenedValue = []; //for accumulating and comparing opened cards
+    this.setTimer = 'No timer';
     this.state = {
+      cardLimitation: 0, //limitation on opening cards, no more than 2
       imageNumber: [],
-      identifier: 0,
-      imageOfOpenedCards: []
+      imageOfOpenedCards: [],
+      startGame: 'before start: cards not available',
+      remainingTime: 'No timer',
+      buttonClasses: ['no-timer', 'timer30', 'timer60'],
+      activeButton: 'No timer',
+      countPairsOfOpenedCards: 1,  // number of opened card pairs
     }
     this.startGame = this.startGame.bind(this);
-    this.timer = this.timer.bind(this);
+    this.timeToRemember = this.timeToRemember.bind(this);
     this.openCloseCards = this.openCloseCards.bind(this);
+    this.timerOfGame = this.timerOfGame.bind(this);
+    this.chooseTimer = this.chooseTimer.bind(this);
   }
-  // раскладка карт при нажатии кнопки "Начать игру"
-  startGame () {
+
+  //сhoose timer settings
+  chooseTimer (e) {
+    if (this.state.startGame !== 'user is playing') {
+      this.setState({
+        activeButton: e.target.getAttribute('data'),
+        remainingTime: e.target.getAttribute('data')
+      })
+    }
+  }
+
+  //user sees which timer button is pressed
+  isActiveButton(buttonId) {
+    return this.state.activeButton === buttonId ? 'clicked-timer' : '';
+  }
+
+  //timer setting
+  timerOfGame (e) {
+    if (this.state.remainingTime === 'No timer'){
+      return;
+    }
+    clearInterval(timer);
+    timer = setInterval(() => {
+      if(!this.state.remainingTime) {
+        clearInterval(timer);
+        return false;
+      }
+      this.setState(prevState => {
+        return {remainingTime: prevState.remainingTime - 1}
+      });
+    }, 1000)
+  }
+
+  // the layout of the cards when you click the "Start game" button
+  startGame (e) {
+    this.setState({startGame: 'game began: cards are laid out'});
     let arrayOfImageNumber = [];
     for (let k = 1; k<13; k++){
       arrayOfImageNumber.push(k);
     }
-    //Тасование Фишера — Йетса
+    this.shuffle(arrayOfImageNumber);
+    this.timeToRemember(arrayOfImageNumber);
+  }
+
+  // Fisher-Yates Shuffle
+  shuffle (arrayOfImageNumber) {
     for (let i = arrayOfImageNumber.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [arrayOfImageNumber[i], arrayOfImageNumber[j]] = [arrayOfImageNumber[j], arrayOfImageNumber[i]];
@@ -29,90 +75,107 @@ class App extends React.Component {
     }
     arrayOfImageNumber[0] = arrayOfImageNumber[0] > 6 ? (arrayOfImageNumber.length + 1 - arrayOfImageNumber[0]) : arrayOfImageNumber[0];
     this.setState({imageNumber: arrayOfImageNumber});
-    this.timer(arrayOfImageNumber);
   }
 
-  timer (arrayOfImageNumber) {
+  timeToRemember (arrayOfImageNumber) {
     setTimeout (() => {
-    for (let i = 0; i < arrayOfImageNumber.length; i++) {
-      arrayOfImageNumber[i] = arrayOfImageNumber[i] + 'closed';
-    }
-    this.setState({imageNumber: arrayOfImageNumber})}, 2000);
-}
+      for (let i = 0; i < arrayOfImageNumber.length; i++) {
+        arrayOfImageNumber[i] = arrayOfImageNumber[i] + 'closed';
+      }
+      this.setState({
+        imageNumber: arrayOfImageNumber,
+        startGame: 'user is playing',
+      });
+      this.timerOfGame();
+    }, 4000);
+  }
 
   openCloseCards (e) {
-      if (this.state.identifier < 2) {
+    console.log('clicked index = ' + e.target.getAttribute('data-index'));
+    if (this.state.startGame === 'user is playing') {
+     if (this.state.cardLimitation < 2) {
         let index = e.target.getAttribute('data-index');
         let value = parseInt(this.state.imageNumber[index]);
-        this.value.push(value);
+        this.twoOpenedValue.push(value);
         this.setState({
           imageNumber: {
             ...this.state.imageNumber,
             [index]: value,
           },
-          identifier: this.state.identifier + 1,
+          cardLimitation: this.state.cardLimitation + 1,
         })
-        // если две открытые карточки равны
-        if (this.value.length === 2 ) {
+        // if two open cards are equal
+        if (this.twoOpenedValue.length === 2 ) {
           console.log(this.state.imageNumber);
-          if (this.value[0] === this.value[1]) {
+          if (this.twoOpenedValue[0] === this.twoOpenedValue[1]) {
             this.setState({
-              identifier: 0,
-              // imageNumber: {
-              //   ...this.state.imageNumber,
-              //   [index]: value + 'opened',
-              // },
-            })
-
-            this.value = [];
-            console.log(this.value[0] === this.value[1]);
+              countPairsOfOpenedCards: this.state.countPairsOfOpenedCards + 1,
+              cardLimitation: 0,
+            });
+            this.twoOpenedValue = [];
+            console.log(this.state.countPairsOfOpenedCards);
           } else {
-            // если две открытые карточки НЕ равны, нужно их снова скрыть
-            setTimeout (() => {
-              for (let imageNumber in this.state.imageNumber) {
-                // if (!this.state.imageNumber[imageNumber].length > 1) {
-                  console.log(this.state.imageNumber[imageNumber]);
+              // if two open cards are NOT equal, you need to hide them again
+              setTimeout (() => {
+                for (let imageNumber in this.state.imageNumber) {
                   const valueImageNumber = this.state.imageNumber[imageNumber];
-                this.setState({
-                  imageNumber: {
-                    ...this.state.imageNumber,
-                    [imageNumber]: valueImageNumber === this.value[0] || valueImageNumber === this.value[1] ? this.state.imageNumber[imageNumber] + 'closed' : this.state.imageNumber[imageNumber] 
-                  },
-                  identifier: 0,
-                })
-              // }
+                  this.setState({
+                    imageNumber: {
+                      ...this.state.imageNumber,
+                      [imageNumber]: valueImageNumber === this.twoOpenedValue[0] || valueImageNumber === this.twoOpenedValue[1] ? this.state.imageNumber[imageNumber] + 'closed' : this.state.imageNumber[imageNumber]
+                    },
+                    cardLimitation: 0,
+                  })
+                }
+                this.twoOpenedValue = [];
+              }, 2000);
             }
-              this.value = [];
-              console.log(this.state.imageNumber, 'this.state.identifier', this.state.identifier);
-            }, 2000);
-          }
         }
-        console.log(this.state.imageNumber[index], 'index:', e.target.getAttribute('data-index'), 'value:', value);
-        console.log('this.value.length:', this.value.length, this.value[0] === this.value[1]);
+      } 
     }
   }
-
+  
+  addClassNotAvailable () {
+    return (this.state.startGame === 'before start: cards not available' ? " not-available" : '');
+  }
+  
   render() {
     return (
-      <div className="App">
-        <button onClick={this.startGame}>Начать игру</button>
+      <div className="App" onClick={this.howStartGame}>
+        <div className="app-top">
+          <button className="button-start" onClick={this.startGame}>Start game</button>
+          <div className={"popap-to-start" + (this.state.startGame !== 'before start: cards not available' ? " disappearance" : "")}>
+            To start the game, click on the "Start Game"
+          </div>
+          <div className="customization">
+            <p>Game settings</p>
+            <div className="timer-game">
+              <p>Timer: {this.state.remainingTime}</p>
+              <button data='No timer' className={this.isActiveButton('No timer')} onClick={this.chooseTimer}>No timer</button>
+              <button data="30" className={this.isActiveButton('30')} onClick={this.chooseTimer}>30 seconds</button>
+              <button data="60" className={this.isActiveButton('60')} onClick={this.chooseTimer}>60 seconds</button>
+          </div>
+          </div>
+        </div>
         <div className="block block-1">
-          <div className={"image image" + this.state.imageNumber[0]} onClick={this.openCloseCards} data-index="0"></div>
-          <div className={"image image" + this.state.imageNumber[1]} onClick={this.openCloseCards} data-index="1"></div>
-          <div className={"image image" + this.state.imageNumber[2]} onClick={this.openCloseCards} data-index="2"></div>
-          <div className={"image image" + this.state.imageNumber[3]} onClick={this.openCloseCards} data-index="3"></div>
+          <div className={"image image" + this.state.imageNumber[0] + " " + this.addClassNotAvailable()}
+            onClick={this.openCloseCards} data-index="0">
+          </div>
+          <div className={"image image" + this.state.imageNumber[1] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="1"></div>
+          <div className={"image image" + this.state.imageNumber[2] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="2"></div>
+          <div className={"image image" + this.state.imageNumber[3] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="3"></div>
         </div>
         <div className="block block-2">
-          <div className={"image image" + this.state.imageNumber[4]} onClick={this.openCloseCards} data-index="4"></div>
-          <div className={"image image" + this.state.imageNumber[5]} onClick={this.openCloseCards} data-index="5"></div>
-          <div className={"image image" + this.state.imageNumber[6]} onClick={this.openCloseCards} data-index="6"></div>
-          <div className={"image image" + this.state.imageNumber[7]} onClick={this.openCloseCards} data-index="7"></div>
+          <div className={"image image" + this.state.imageNumber[4] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="4"></div>
+          <div className={"image image" + this.state.imageNumber[5] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="5"></div>
+          <div className={"image image" + this.state.imageNumber[6] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="6"></div>
+          <div className={"image image" + this.state.imageNumber[7] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="7"></div>
         </div>
         <div className="block block-3">
-          <div className={"image image" + this.state.imageNumber[8]} onClick={this.openCloseCards} data-index="8"></div>
-          <div className={"image image" + this.state.imageNumber[9]} onClick={this.openCloseCards} data-index="9"></div>
-          <div className={"image image" + this.state.imageNumber[10]} onClick={this.openCloseCards} data-index="10"></div>
-          <div className={"image image" + this.state.imageNumber[11]} onClick={this.openCloseCards} data-index="11"></div>
+          <div className={"image image" + this.state.imageNumber[8] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="8"></div>
+          <div className={"image image" + this.state.imageNumber[9] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="9"></div>
+          <div className={"image image" + this.state.imageNumber[10] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="10"></div>
+          <div className={"image image" + this.state.imageNumber[11] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="11"></div>
         </div>
       </div>
     );
@@ -120,68 +183,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-
-// import React from 'react';
-// import './App.css';
-
-// class App extends React.Component {
-//   constructor (props) {
-//     super(props);
-//     this.adressOfImages = [];
-//     this.state = {
-//       class: Array(12).fill(null),
-//     }
-//     this.startGame = this.startGame.bind(this);
-//   }
-//   startGame (e) {
-//     let array = [];
-//     for (let k = 1; k<13; k++){
-//       array.push(k);
-//     }
-//     //Тасование Фишера — Йетса
-//     for (let i = array.length - 1; i > 0; i--) {
-//       let j = Math.floor(Math.random() * (i + 1));
-//       [array[i], array[j]] = [array[j], array[i]];
-//     }
-//     console.log(e.target);
-//     this.fillingWithPictures(e, array);
-//   }
-
-//   fillingWithPictures (e, arr) {
-//     // let images = document.querySelectorAll('.image');
-//     for (let i = 0; i < arr.length; i++) {
-//       // this.state.class.setState
-//       // images[i].classList = arr[i] < 7 ? 'image image' + arr[i] : 'image image' + (arr.length + 1 - arr[i]);
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <button onClick={this.startGame}>Начать игру</button>
-//         <div className="block block-1">
-//           <div className="image" data="1"></div>
-//           <div className="image" data="2"></div>
-//           <div className="image" data="3"></div>
-//           <div className="image" data="4"></div>
-//         </div>
-//         <div className="block block-2">
-//           <div className="image"></div>
-//           <div className="image"></div>
-//           <div className="image"></div>
-//           <div className="image"></div>
-//         </div>
-//         <div className="block block-3">
-//           <div className="image"></div>
-//           <div className="image"></div>
-//           <div className="image"></div>
-//           <div className="image"></div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
