7ae2a5b28d6ae3365d7098ae0d037a12fc8c8eee
MarinaYur <marina.yurkev@gmail.com>
2021-02-25 13:31:54 +0300

    feat: complete the game ending logic

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
       setNumberOfCards: '12',
       backgroundColor: 'white',
       countPairsOfOpenedCards: 1,  // number of opened card pairs
+      popapMessage: 'To start the game, click on the "Start Game"',
     }
     this.startGame = this.startGame.bind(this);
     this.timeToRemember = this.timeToRemember.bind(this);
@@ -22,11 +23,13 @@ class App extends React.Component {
     this.chooseTimer = this.chooseTimer.bind(this);
     this.chooseNumberOfCards = this.chooseNumberOfCards.bind(this);
     this.setBackgroundColor = this.setBackgroundColor.bind(this);
+    this.hideExtraCards = this.hideExtraCards.bind(this);
   }
 
   //сhoose timer settings
   chooseTimer (e) {
-    if (this.state.startGame !== 'user is playing') {
+    console.log('this.state.startGame', this.state.startGame);
+    if (this.state.startGame === 'before start: cards not available') {
       this.setTimer = e.target.getAttribute('data');
       this.setState({
         remainingTime: e.target.getAttribute('data')
@@ -36,17 +39,17 @@ class App extends React.Component {
 
   //сhoose settings number of cards
   chooseNumberOfCards (e) {
-    if (this.state.startGame !== 'user is playing') {
+    if (this.state.startGame === 'before start: cards not available') {
       this.setState({setNumberOfCards: e.target.getAttribute('data')});
     }
   }
 
   setBackgroundColor (e) {
-    if (this.state.startGame !== 'user is playing') {
+    console.log('this.state.startGame', this.state.startGame);
+    if (this.state.startGame === 'before start: cards not available') {
       this.setState({backgroundColor: e.target.getAttribute('data')});
+      e.target.closest('.App').style.background = this.state.backgroundColor;
     }
-    e.target.closest('.App').style.background = this.state.backgroundColor;
-    // console.log('background-color:' + this.state.backgroundColor);
   }
 
 
@@ -55,13 +58,22 @@ class App extends React.Component {
     return this.setTimer === buttonId ? 'clicked-timer' : '';
   }
 
+  //user sees which number button is pressed
   isActiveButtonNumber(buttonId) {
     return this.state.setNumberOfCards === buttonId ? 'clicked-timer' : '';
   }
 
+  //user sees which color was selected
   isActiveButtonColor(buttonId) {
     return this.state.backgroundColor === buttonId ? 'clicked-timer' : '';
   }
+  
+  //remove extra cards if option 12 is selected instead of 18
+  hideExtraCards () {
+    return {
+      display: this.state.setNumberOfCards === '12' ? 'none' : 'flex'
+    }
+  };
 
   //timer setting
   timerOfGame (e) {
@@ -74,22 +86,23 @@ class App extends React.Component {
         clearInterval(timer);
         return false;
       }
-      this.setState(prevState => {
-        return {remainingTime: prevState.remainingTime - 1}
+      this.setState({
+        remainingTime: this.state.remainingTime - 1
       });
     }, 1000)
   }
 
   // the layout of the cards when you click the "Start game" button
   startGame (e) {
-    this.setState({startGame: 'game began: cards are laid out'});
-    let arrayOfImageNumber = [];
-    let number = this.state.setNumberOfCards;
-    for (let k = 1; k <= number; k++){
-      arrayOfImageNumber.push(k);
+    if (this.state.startGame !== 'user is playing') {
+      let arrayOfImageNumber = [];
+      let number = this.state.setNumberOfCards;
+      for (let k = 1; k <= number; k++){
+        arrayOfImageNumber.push(k);
+      }
+      this.shuffle(arrayOfImageNumber, number);
+      this.timeToRemember(arrayOfImageNumber);
     }
-    this.shuffle(arrayOfImageNumber, number);
-    this.timeToRemember(arrayOfImageNumber);
   }
 
   // Fisher-Yates Shuffle
@@ -100,7 +113,10 @@ class App extends React.Component {
       arrayOfImageNumber[i] = arrayOfImageNumber[i] > number/2 ? arrayOfImageNumber.length + 1 - arrayOfImageNumber[i] : arrayOfImageNumber[i];
     }
     arrayOfImageNumber[0] = arrayOfImageNumber[0] > number/2 ? (arrayOfImageNumber.length + 1 - arrayOfImageNumber[0]) : arrayOfImageNumber[0];
-    this.setState({imageNumber: arrayOfImageNumber});
+    this.setState({
+      imageNumber: arrayOfImageNumber,
+      startGame: 'user is playing',
+      });
   }
 
   timeToRemember (arrayOfImageNumber) {
@@ -110,57 +126,86 @@ class App extends React.Component {
       }
       this.setState({
         imageNumber: arrayOfImageNumber,
-        startGame: 'user is playing',
       });
       this.timerOfGame();
     }, 4000);
   }
 
   openCloseCards (e) {
-    console.log('clicked index = ' + e.target.getAttribute('data-index'));
-    if (this.state.startGame === 'user is playing') {
-     if (this.state.cardLimitation < 2) {
-        let index = e.target.getAttribute('data-index');
-        let value = parseInt(this.state.imageNumber[index]);
-        this.twoOpenedValue.push(value);
-        this.setState({
-          imageNumber: {
-            ...this.state.imageNumber,
-            [index]: value,
-          },
-          cardLimitation: this.state.cardLimitation + 1,
-        })
-        // if two open cards are equal
-        if (this.twoOpenedValue.length === 2 ) {
-          console.log(this.state.imageNumber);
-          if (this.twoOpenedValue[0] === this.twoOpenedValue[1]) {
-            this.setState({
-              countPairsOfOpenedCards: this.state.countPairsOfOpenedCards + 1,
-              cardLimitation: 0,
-            });
-            this.twoOpenedValue = [];
-            console.log(this.state.countPairsOfOpenedCards);
-          } else {
-              // if two open cards are NOT equal, you need to hide them again
-              setTimeout (() => {
-                for (let imageNumber in this.state.imageNumber) {
-                  const valueImageNumber = this.state.imageNumber[imageNumber];
-                  this.setState({
-                    imageNumber: {
-                      ...this.state.imageNumber,
-                      [imageNumber]: valueImageNumber === this.twoOpenedValue[0] || valueImageNumber === this.twoOpenedValue[1] ? this.state.imageNumber[imageNumber] + 'closed' : this.state.imageNumber[imageNumber]
-                    },
-                    cardLimitation: 0,
-                  })
-                }
+    if (this.state.remainingTime !==0 || this.state.remainingTime === 'No timer') {
+      if (this.state.startGame === 'user is playing') {
+        if (this.state.cardLimitation < 2) {
+          let index = e.target.getAttribute('data-index');
+          let value = parseInt(this.state.imageNumber[index]);
+          this.twoOpenedValue.push(value);
+          this.setState({
+            imageNumber: {
+              ...this.state.imageNumber,
+              [index]: value,
+            },
+            cardLimitation: this.state.cardLimitation + 1,
+          })
+          // if two open cards are equal
+            if (this.twoOpenedValue.length === 2 ) {
+              if (this.twoOpenedValue[0] === this.twoOpenedValue[1]) {
+                this.setState({
+                  countPairsOfOpenedCards: this.state.countPairsOfOpenedCards + 1,
+                  cardLimitation: 0,
+                });
                 this.twoOpenedValue = [];
-              }, 2000);
+                if (this.state.countPairsOfOpenedCards === this.state.setNumberOfCards / 2) {
+                  setTimeout(() => 
+                  this.setState({
+                    startGame: 'before start: cards not available',
+                    popapMessage: 'To start the new game, click on the "Start Game"',
+                    imageNumber: []
+                  }), 3000);
+                  this.setState({
+                    countPairsOfOpenedCards: 1,
+                    startGame: 'before start: cards not available',
+                    popapMessage: 'Excellent. You won',
+                    remainingTime: 'No timer',
+                  })
+                  console.log('this.state.countPairsOfOpenedCards :', this.state.countPairsOfOpenedCards);
+                  console.log('this.state.cardLimitation:', this.state.cardLimitation);
+                  console.log(this.state.imageNumber);
+                console.log(this.state.countPairsOfOpenedCards);
+                }
+              } else {
+                  // if two open cards are NOT equal, you need to hide them again
+                  setTimeout (() => {
+                    for (let imageNumber in this.state.imageNumber) {
+                      const valueImageNumber = this.state.imageNumber[imageNumber];
+                      this.setState({
+                        imageNumber: {
+                          ...this.state.imageNumber,
+                          [imageNumber]: valueImageNumber === this.twoOpenedValue[0] || valueImageNumber === this.twoOpenedValue[1] ? this.state.imageNumber[imageNumber] + 'closed' : this.state.imageNumber[imageNumber]
+                        },
+                        cardLimitation: 0,
+                      })
+                    }
+                    this.twoOpenedValue = [];
+                  }, 2000);
+                }
             }
+          } 
         }
-      } 
+    } else 
+    if (this.state.remainingTime === 0) {
+      this.setState({
+        startGame: 'before start: cards not available',
+        popapMessage: 'Unfortunately, you did not meet the deadline',
+        countPairsOfOpenedCards: 1,
+      })
+      setTimeout(() => 
+                  this.setState({
+                    popapMessage: 'But you can try again, click on the "Start Game"',
+                    imageNumber: [],
+                    remainingTime: this.setTimer,
+                  }), 3000);
     }
   }
-  
+
   addClassNotAvailable () {
     return (this.state.startGame === 'before start: cards not available' ? " not-available" : '');
   }
@@ -170,9 +215,16 @@ class App extends React.Component {
       <div className="App">
         <div className="app-top">
           <button className="button-start" onClick={this.startGame}>Start game</button>
-          <div className={"popap-to-start" + (this.state.startGame !== 'before start: cards not available' ? " disappearance" : "")}>
-            To start the game, click on the "Start Game"
+          <div className={"popap" + (this.state.startGame !== 'before start: cards not available' ? " disappearance" : "")}>
+            {this.state.popapMessage}
           </div>
+
+          <div className="popap-finish">
+            <div className="popap-finish__title"></div>
+            <button className="popap-finish__button1">Yes</button>
+            <button className="popap-finish__button2">No</button>
+          </div>
+
           <div className="customization">
             <p className = "customization_title">Game settings</p>
             <div className="settings">
@@ -211,7 +263,7 @@ class App extends React.Component {
           <div className={"image image" + this.state.imageNumber[10] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="10"></div>
           <div className={"image image" + this.state.imageNumber[11] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="11"></div>
         </div>
-        <div className="block block-3">
+        <div className="block block-3" style={this.hideExtraCards()}>
           <div className={"image image" + this.state.imageNumber[12] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="12"></div>
           <div className={"image image" + this.state.imageNumber[13] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="13"></div>
           <div className={"image image" + this.state.imageNumber[14] + " " + this.addClassNotAvailable()} onClick={this.openCloseCards} data-index="14"></div>
